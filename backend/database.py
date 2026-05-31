27f1059852b6262eeaf2b615917f233dd808a2fa
Kaviya <kaavyagnanasekaran@gmail.com>
2026-05-31 22:16:55 +0530

    Updated database

--- backend/database.py
+++ backend/database.py
@@ -17,7 +17,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:admin123@localhost:5434/mavx"
+)
 
 engine = create_engine(DATABASE_URL)
 
