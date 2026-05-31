3a8b108ac80669f5f38a19fee1f9ccb1d831f7ba
Kaviya <kaavyagnanasekaran@gmail.com>
2026-05-31 22:03:07 +0530

    database file edited

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-
+""" 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,5 +8,23 @@ DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/mavx"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+ """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
